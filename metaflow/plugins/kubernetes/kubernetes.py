52475e91686544ee4c830a7f3ad2226d63a583d1
Oleg Avdeev <oleg.v.avdeev@gmail.com>
2021-12-10 19:57:34 -0800

    [tracing] opentelemetry support

--- metaflow/plugins/kubernetes/kubernetes.py
+++ metaflow/plugins/kubernetes/kubernetes.py
@@ -33,6 +33,7 @@ from metaflow.metaflow_config import (
     SERVICE_HEADERS,
     SERVICE_INTERNAL_URL,
     S3_SERVER_SIDE_ENCRYPTION,
+    OTEL_ENDPOINT,
 )
 from metaflow.mflog import (
     BASH_SAVE_LOGS,
@@ -252,6 +253,7 @@ class Kubernetes(object):
             .environment_variable(
                 "METAFLOW_INIT_SCRIPT", KUBERNETES_SANDBOX_INIT_SCRIPT
             )
+            .environment_variable("METAFLOW_OTEL_ENDPOINT", OTEL_ENDPOINT)
             # Skip setting METAFLOW_DATASTORE_SYSROOT_LOCAL because metadata sync
             # between the local user instance and the remote Kubernetes pod
             # assumes metadata is stored in DATASTORE_LOCAL_DIR on the Kubernetes
